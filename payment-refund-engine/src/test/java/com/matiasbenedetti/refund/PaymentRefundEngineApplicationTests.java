4f0beccf0661b80cd5f73d5e7a661022a08555ae
Matias Benedetti <benedettimaty@gmail.com>
2026-05-30 23:05:17 -0300

    chore: initial project setup with Spring Boot 4.0.6 and Maven wrapper

---
+++ payment-refund-engine/src/test/java/com/matiasbenedetti/refund/PaymentRefundEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasbenedetti.refund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentRefundEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
